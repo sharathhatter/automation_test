777acf3da0b982b06e83ad7cddeabc469bfd2e28
Siddharth Srivastava <sidmkp96@gmail.com>
2014-11-27 13:12:50 +0530

    BB-2390 : Tweaks to SKU deck for large screens

--- app/src/main/java/com/bigbasket/mobileapp/fragment/base/ProductListAwareFragment.java
+++ app/src/main/java/com/bigbasket/mobileapp/fragment/base/ProductListAwareFragment.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
