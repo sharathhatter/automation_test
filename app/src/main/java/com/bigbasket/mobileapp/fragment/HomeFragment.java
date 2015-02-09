7e4d29d3aec4104b3997e4d41fdab887896df69c
jugal <jugal@bigbasket.com>
2015-02-09 08:57:12 +0530

    WalletActivityListAdapter context related changes

--- app/src/main/java/com/bigbasket/mobileapp/fragment/HomeFragment.java
+++ app/src/main/java/com/bigbasket/mobileapp/fragment/HomeFragment.java
@@ -65,7 +65,7 @@ public class HomeFragment extends BaseSectionFragment implements DynamicScreenAw
             new GetCartCountTask<>(getCurrentActivity(), true).startTask();
             requestHomePage();
         }
-        getAppInfo();
+        //getAppInfo();
         handler = new HomePageHandler<>(this);
     }
 
