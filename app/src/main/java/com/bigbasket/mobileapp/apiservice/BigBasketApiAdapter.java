f1c9fd704d5d5b2e37fe3770d54ccbaec40b8096
Siddharth Srivastava <sidmkp96@gmail.com>
2015-02-20 12:53:40 +0530

    BB-2390 : Removed okhttp, AGAIN!

--- app/src/main/java/com/bigbasket/mobileapp/apiservice/BigBasketApiAdapter.java
+++ app/src/main/java/com/bigbasket/mobileapp/apiservice/BigBasketApiAdapter.java
@@ -7,13 +7,14 @@ import android.text.TextUtils;
 import com.bigbasket.mobileapp.model.request.AuthParameters;
 import com.bigbasket.mobileapp.util.MobileApiUrl;
 import com.bigbasket.mobileapp.util.UIUtil;
-import com.squareup.okhttp.OkHttpClient;
 
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
-import retrofit.client.OkClient;
+import retrofit.client.Request;
+import retrofit.client.UrlConnectionClient;
 
 public class BigBasketApiAdapter {
 
@@ -69,27 +70,28 @@ public class BigBasketApiAdapter {
             }
         };
 
-        OkHttpClient okHttpClient = new OkHttpClient();
-        okHttpClient.setReadTimeout(45, TimeUnit.SECONDS);
-        okHttpClient.setConnectTimeout(20, TimeUnit.SECONDS);
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//        okHttpClient.setReadTimeout(45, TimeUnit.SECONDS);
+//        okHttpClient.setConnectTimeout(20, TimeUnit.SECONDS);
 
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(MobileApiUrl.URL)
                 .setRequestInterceptor(requestInterceptor)
-                .setClient(new OkClient(okHttpClient))
+                .setClient(new BigBasketHttpClient())
                 .build();
 
         //.setClient(new BigBasketHttpClient())
+        //.setClient(new OkClient(okHttpClient))
         bigBasketApiService = restAdapter.create(BigBasketApiService.class);
     }
 
-//    public static final class BigBasketHttpClient extends UrlConnectionClient {
-//        @Override
-//        protected HttpURLConnection openConnection(Request request) throws IOException {
-//            HttpURLConnection httpURLConnection = super.openConnection(request);
-//            httpURLConnection.setConnectTimeout(20 * 1000);
-//            httpURLConnection.setReadTimeout(45 * 1000);
-//            return httpURLConnection;
-//        }
-//    }
+    public static final class BigBasketHttpClient extends UrlConnectionClient {
+        @Override
+        protected HttpURLConnection openConnection(Request request) throws IOException {
+            HttpURLConnection httpURLConnection = super.openConnection(request);
+            httpURLConnection.setConnectTimeout(20 * 1000);
+            httpURLConnection.setReadTimeout(45 * 1000);
+            return httpURLConnection;
+        }
+    }
 }
