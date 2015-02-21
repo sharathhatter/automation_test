1e04ed05c995a5710ad6366e5c2fd751e778c2cb
Siddharth Srivastava <sidmkp96@gmail.com>
2015-02-21 10:15:27 +0530

    BB-2390 : Migrated OTP dialog to use MaterialDialogs

--- app/src/main/java/com/bigbasket/mobileapp/fragment/account/OTPValidationDialogFragment.java
+++ app/src/main/java/com/bigbasket/mobileapp/fragment/account/OTPValidationDialogFragment.java
@@ -4,20 +4,19 @@ import android.app.Dialog;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.bigbasket.mobileapp.R;
 import com.bigbasket.mobileapp.activity.base.BaseActivity;
 import com.bigbasket.mobileapp.interfaces.OtpDialogAware;
+import com.bigbasket.mobileapp.util.FontHolder;
+import com.bigbasket.mobileapp.util.UIUtil;
 
 public class OTPValidationDialogFragment extends DialogFragment {
 
@@ -31,91 +30,61 @@ public class OTPValidationDialogFragment extends DialogFragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.uiv3_otp_dialog, container, false);
-    }
-
-    @Override
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        dialog.setCancelable(false);
-        return dialog;
-    }
+        View view = getActivity().getLayoutInflater().inflate(R.layout.uiv3_otp_dialog, null);
 
+        FontHolder fontHolder = FontHolder.getInstance(getActivity());
+        TextView txtDialogTitle = (TextView) view.findViewById(R.id.txtDialogTitle);
+        txtDialogTitle.setTypeface(fontHolder.getFaceRobotoRegular(), Typeface.BOLD);
+        TextView txtHeaderMsg = (TextView) view.findViewById(R.id.txtHeaderMsg);
+        final EditText editTextMobileCode = (EditText) view.findViewById(R.id.editTextMobileCode);
+        editTextMobileCode.setTypeface(fontHolder.getFaceRobotoRegular());
+        editTextMobileCode.requestFocus();
+        txtHeaderMsg.setTypeface(fontHolder.getFaceRobotoRegular());
+        BaseActivity.showKeyboard(editTextMobileCode);
+        txtErrorValidateNumber = (TextView) view.findViewById(R.id.txtErrorValidateNumber);
+        txtErrorValidateNumber.setTypeface(fontHolder.getFaceRobotoRegular());
+        txtResendNumber = (TextView) view.findViewById(R.id.txtResendNumber);
+        txtResendNumber.setTypeface(fontHolder.getFaceRobotoRegular());
+        TextView txtResendCode = (TextView) view.findViewById(R.id.txtResendCode);
+        txtResendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtErrorValidateNumber.setVisibility(View.GONE);
+                txtResendNumber.setVisibility(View.VISIBLE);
+                resendOrConfirmOTP(null);
+            }
+        });
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        View view = getView();
-        if (view != null) {
-            TextView txtDialogTitle = (TextView) view.findViewById(R.id.txtDialogTitle);
-            txtDialogTitle.setTypeface(BaseActivity.faceRobotoRegular, Typeface.BOLD);
-            TextView txtHeaderMsg = (TextView) view.findViewById(R.id.txtHeaderMsg);
-            final EditText editTextMobileCode = (EditText) view.findViewById(R.id.editTextMobileCode);
-            editTextMobileCode.setTypeface(BaseActivity.faceRobotoRegular);
-            editTextMobileCode.requestFocus();
-            txtHeaderMsg.setTypeface(BaseActivity.faceRobotoRegular);
-            BaseActivity.showKeyboard(editTextMobileCode);
-            txtErrorValidateNumber = (TextView) view.findViewById(R.id.txtErrorValidateNumber);
-            txtErrorValidateNumber.setTypeface(BaseActivity.faceRobotoRegular);
-            txtResendNumber = (TextView) view.findViewById(R.id.txtResendNumber);
-            txtResendNumber.setTypeface(BaseActivity.faceRobotoRegular);
-            TextView txtResendCode = (TextView) view.findViewById(R.id.txtResendCode);
-            txtResendCode.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    txtErrorValidateNumber.setVisibility(View.GONE);
-                    txtResendNumber.setVisibility(View.VISIBLE);
-                    resendOrConfirmOTP(null);
-                }
-            });
-            final TextView btnConfirm = (TextView) view.findViewById(R.id.txtConfirm);
-
-            btnConfirm.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                        btnConfirm.setBackgroundColor(getResources().getColor(R.color.light_blue));
-                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                        btnConfirm.setBackgroundColor(getResources().getColor(R.color.white));
-                        if (TextUtils.isEmpty(editTextMobileCode.getText().toString())) {
-                        } else if (editTextMobileCode.getText() != null && editTextMobileCode.getText().toString().length() > 0) {
-                            resendOrConfirmOTP(editTextMobileCode.getText().toString());
-                        } else {
-                            txtErrorValidateNumber.setText(getResources().getString(R.string.otpCodeErrorMsg));
-                            txtErrorValidateNumber.setVisibility(View.VISIBLE);
-                            txtResendNumber.setVisibility(View.GONE);
+        MaterialDialog.Builder builder = UIUtil.getMaterialDialogBuilder(getActivity())
+                .autoDismiss(false)
+                .cancelable(false)
+                .customView(view, false)
+                .callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        if (!TextUtils.isEmpty(editTextMobileCode.getText().toString())) {
+                            if (editTextMobileCode.getText() != null && editTextMobileCode.getText().toString().length() > 0) {
+                                resendOrConfirmOTP(editTextMobileCode.getText().toString());
+                            } else {
+                                txtErrorValidateNumber.setText(getResources().getString(R.string.otpCodeErrorMsg));
+                                txtErrorValidateNumber.setVisibility(View.VISIBLE);
+                                txtResendNumber.setVisibility(View.GONE);
+                            }
                         }
                     }
-                    return true;
-                }
-            });
 
-
-            final TextView txtCancel = (TextView) view.findViewById(R.id.txtCancel);
-            txtCancel.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                        txtCancel.setBackgroundColor(getResources().getColor(R.color.light_blue));
-                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                        txtCancel.setBackgroundColor(getResources().getColor(R.color.white));
-                        if (getDialog().isShowing())
+                    @Override
+                    public void onNegative(MaterialDialog dialog) {
+                        if (getDialog().isShowing()) {
                             getDialog().dismiss();
+                        }
                     }
-                    return true;
-                }
-            });
-
-        }
+                });
+        Dialog dialog = builder.build();
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
     }
 
     public void showErrorText(String errorMsg) {
