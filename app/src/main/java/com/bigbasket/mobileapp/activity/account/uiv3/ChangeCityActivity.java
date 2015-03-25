558f8736fed026b68550e8dcfe76e1215e6661cd
Siddharth Srivastava <sidmkp96@gmail.com>
2015-03-25 19:04:07 +0530

    BB-3791 : Fixes to change city

--- app/src/main/java/com/bigbasket/mobileapp/activity/account/uiv3/ChangeCityActivity.java
+++ app/src/main/java/com/bigbasket/mobileapp/activity/account/uiv3/ChangeCityActivity.java
@@ -62,7 +62,7 @@ public class ChangeCityActivity extends BackButtonActivity implements CityListDi
                 android.R.layout.simple_list_item_single_choice, cities);
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         listView.setAdapter(citySpinnerAdapter);
-        listView.setItemChecked(0, true);
+        listView.setItemChecked(findCurrentCityPosition(cities), true);
 
         FloatingActionButton btnFab = (FloatingActionButton) base.findViewById(R.id.btnFab);
         btnFab.setImageResource(R.drawable.ic_keyboard_arrow_right_white_36dp);
@@ -78,6 +78,17 @@ public class ChangeCityActivity extends BackButtonActivity implements CityListDi
         });
 
         contentFrame.addView(base);
+    }
+
+    private int findCurrentCityPosition(ArrayList<City> cities) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String currentCityName = preferences.getString(Constants.CITY, null);
+        for (int i = 0; i < cities.size(); i++) {
+            if (cities.get(i).getName().equals(currentCityName)) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     protected void changeCity(final City city) {
