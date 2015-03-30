71d8765232f42202d730d0f7448087253c745e98
Siddharth Srivastava <sidmkp96@gmail.com>
2015-03-30 12:38:41 +0530

    BB-00 - Incremented db version

--- app/src/main/java/com/bigbasket/mobileapp/adapter/db/DatabaseHelper.java
+++ app/src/main/java/com/bigbasket/mobileapp/adapter/db/DatabaseHelper.java
@@ -16,7 +16,7 @@ import com.bigbasket.mobileapp.adapter.product.SubCategoryAdapter;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "bigbasket.db";
-    protected static final int DATABASE_VERSION = 10;
+    protected static final int DATABASE_VERSION = 11;
     public static SQLiteDatabase db = null;
     private static DatabaseHelper dbAdapter = null;
     private static boolean isConnectionOpen = false;
