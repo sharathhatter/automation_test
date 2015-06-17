ac2c07de99b1471f65adaafb7bc2d38f776daa46
Siddharth Srivastava <sidmkp96@gmail.com>
2015-06-17 18:55:29 +0530

    BB-4861 : Changed URL

--- app/src/main/java/com/bigbasket/mobileapp/util/MobileApiUrl.java
+++ app/src/main/java/com/bigbasket/mobileapp/util/MobileApiUrl.java
@@ -1,7 +1,7 @@
 package com.bigbasket.mobileapp.util;
 
 public final class MobileApiUrl {
-    public static final String DOMAIN = "http://192.168.1.172:8000/";
+    public static final String DOMAIN = "http://dev1.bigbasket.com/";
     public static final String URL = DOMAIN + "mapi/v2.0.0";
 
     private MobileApiUrl() {
