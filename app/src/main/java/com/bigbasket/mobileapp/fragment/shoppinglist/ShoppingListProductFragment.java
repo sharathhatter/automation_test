38a072fa9a9f4b27e01eee294eec273d24f6dbbd
jugal <jugal@bigbasket.com>
2015-07-28 13:02:12 +0530

    Fir for shopping list

--- app/src/main/java/com/bigbasket/mobileapp/fragment/shoppinglist/ShoppingListProductFragment.java
+++ app/src/main/java/com/bigbasket/mobileapp/fragment/shoppinglist/ShoppingListProductFragment.java
@@ -59,6 +59,7 @@ public class ShoppingListProductFragment extends ProductListAwareFragment {
                 .setShowShopListDeleteBtn(!shoppingListSummary.getShoppingListName().isSystem())
                 .setShoppingListName(shoppingListSummary.getShoppingListName())
                 .setRupeeTypeface(faceRupee)
+                .showQtyInput(AuthParameters.getInstance(getActivity()).isKirana())
                 .build();
     }
 
