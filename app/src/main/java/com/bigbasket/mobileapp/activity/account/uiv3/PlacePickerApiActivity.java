805aab93a178636598b8ffa6d012af82c341da3f
Siddharth Srivastava <sidmkp96@gmail.com>
2015-08-19 18:29:38 +0530

    BB-5648 : Reading current location from the app

--- app/src/main/java/com/bigbasket/mobileapp/activity/account/uiv3/PlacePickerApiActivity.java
+++ app/src/main/java/com/bigbasket/mobileapp/activity/account/uiv3/PlacePickerApiActivity.java
@@ -1,44 +1,46 @@
 package com.bigbasket.mobileapp.activity.account.uiv3;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.bigbasket.mobileapp.R;
 import com.bigbasket.mobileapp.activity.base.uiv3.BackButtonActivity;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class PlacePickerApiActivity extends BackButtonActivity implements OnMapReadyCallback {
     private GoogleApiClient mGoogleApiClient;
-    private static final int PLACE_PICKER_REQUEST = 1;
+    private Location mLastLocation;
+    private GoogleMap mGoogleMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .addApi(Places.GEO_DATA_API)
-//                .addApi(Places.PLACE_DETECTION_API)
-//                .addConnectionCallbacks(this)
-//                .addOnConnectionFailedListener(this)
-//                .build();
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-
-        GoogleMap googleMap = mapFragment.getMap();
-        googleMap.setMyLocationEnabled(true);
-
+        mapFragment.getMap().setMyLocationEnabled(true);
+        mapFragment.getMap().getUiSettings().setMyLocationButtonEnabled(true);
+        mapFragment.getMap().getUiSettings().setZoomControlsEnabled(true);
         mapFragment.getMapAsync(this);
+        buildGoogleApiClient();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
@@ -49,18 +51,42 @@ public class PlacePickerApiActivity extends BackButtonActivity implements OnMapR
     @Override
     protected void onStart() {
         super.onStart();
-//        mGoogleApiClient.connect();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
 
     @Override
     protected void onStop() {
-//        mGoogleApiClient.disconnect();
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
         super.onStop();
     }
 
     @Override
     public void onConnected(Bundle connectionHint) {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                updateLocationOnMap();
+            }
+        }
+    }
 
+    private void updateLocationOnMap() {
+        LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        Marker mapMarker = mGoogleMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .draggable(true));
+        animateToMarker(mapMarker);
+    }
+
+    private void animateToMarker(Marker marker) {
+        if (mGoogleMap == null) return;
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(marker.getPosition()).zoom(12).build();
+        mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
     @Override
@@ -70,5 +96,6 @@ public class PlacePickerApiActivity extends BackButtonActivity implements OnMapR
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
     }
 }
