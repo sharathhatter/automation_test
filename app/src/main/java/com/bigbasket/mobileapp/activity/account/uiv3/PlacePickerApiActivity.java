81ee3aa3b5adbd077246f9142bd693aa4a80f588
Siddharth Srivastava <sidmkp96@gmail.com>
2015-08-20 14:06:38 +0530

    BB-5648 : WIP

--- app/src/main/java/com/bigbasket/mobileapp/activity/account/uiv3/PlacePickerApiActivity.java
+++ app/src/main/java/com/bigbasket/mobileapp/activity/account/uiv3/PlacePickerApiActivity.java
@@ -3,24 +3,44 @@ package com.bigbasket.mobileapp.activity.account.uiv3;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.AutoCompleteTextView;
 
 import com.bigbasket.mobileapp.R;
 import com.bigbasket.mobileapp.activity.base.uiv3.BackButtonActivity;
+import com.bigbasket.mobileapp.adapter.location.PlaceAutoSuggestAdapter;
+import com.bigbasket.mobileapp.interfaces.PlaceAutoSuggestListener;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PlacePickerApiActivity extends BackButtonActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class PlacePickerApiActivity extends BackButtonActivity implements OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener, PlaceAutoSuggestListener {
+
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
+    @Nullable
     private GoogleMap mGoogleMap;
+    @Nullable
+    private Marker mGoogleMapMarker;
+    private PlaceAutoSuggestAdapter<AutoCompletePlace> mPlaceAutoSuggestAdapter;
+    private ArrayList<AutoCompletePlace> mPlaces;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,17 +48,29 @@ public class PlacePickerApiActivity extends BackButtonActivity implements OnMapR
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMap().setMyLocationEnabled(true);
-        mapFragment.getMap().getUiSettings().setMyLocationButtonEnabled(true);
-        mapFragment.getMap().getUiSettings().setZoomControlsEnabled(true);
+        GoogleMap map = mapFragment.getMap();
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.setOnMyLocationButtonClickListener(this);
         mapFragment.getMapAsync(this);
         buildGoogleApiClient();
+
+        AutoCompleteTextView aEditTextChooseArea =
+                (AutoCompleteTextView) findViewById(R.id.aEditTextChooseArea);
+        mPlaces = new ArrayList<>();
+        mPlaceAutoSuggestAdapter =
+                new PlaceAutoSuggestAdapter<>(this, android.R.layout.simple_list_item_1, mPlaces,
+                        faceRobotoRegular, getResources().getColor(R.color.uiv3_primary_text_color),
+                        getResources().getColor(R.color.uiv3_primary_text_color));
+        aEditTextChooseArea.setAdapter(mPlaceAutoSuggestAdapter);
     }
 
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
+                .addApi(Places.GEO_DATA_API)
                 .addApi(LocationServices.API)
                 .build();
     }
@@ -69,23 +101,40 @@ public class PlacePickerApiActivity extends BackButtonActivity implements OnMapR
         if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
             if (mLastLocation != null) {
-                updateLocationOnMap();
+                updateLastKnownLocationOnMap();
             }
         }
     }
 
-    private void updateLocationOnMap() {
+    private void updateLastKnownLocationOnMap() {
+        if (mGoogleMap == null) return;
         LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-        Marker mapMarker = mGoogleMap.addMarker(new MarkerOptions()
+        mGoogleMapMarker = mGoogleMap.addMarker(new MarkerOptions()
                 .position(latLng)
                 .draggable(true));
-        animateToMarker(mapMarker);
+        mGoogleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                mGoogleMapMarker = marker;
+            }
+        });
+        animateToMarker();
     }
 
-    private void animateToMarker(Marker marker) {
-        if (mGoogleMap == null) return;
+    private void animateToMarker() {
+        if (mGoogleMap == null || mGoogleMapMarker == null) return;
         CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(marker.getPosition()).zoom(12).build();
+                .target(mGoogleMapMarker.getPosition()).zoom(13).build();
         mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
@@ -97,5 +146,68 @@ public class PlacePickerApiActivity extends BackButtonActivity implements OnMapR
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        if (mGoogleMap != null && mGoogleMapMarker != null) {
+            mGoogleMapMarker.remove();
+            updateLastKnownLocationOnMap();
+        }
+        return false;
+    }
+
+    @Override
+    public void displaySuggestion(String constraint) {
+        //Southwest corner to Northeast corner.
+        LatLngBounds bounds = new LatLngBounds(new LatLng(7.43231, 65.82658), new LatLng(36.93593, 99.04924));
+        ArrayList<Integer> filterTypes = new ArrayList<>();
+        filterTypes.add(Place.TYPE_LOCALITY);
+        filterTypes.add(Place.TYPE_SUBLOCALITY);
+        filterTypes.add(Place.TYPE_SUBLOCALITY);
+        filterTypes.add(Place.TYPE_ADMINISTRATIVE_AREA_LEVEL_1);
+        filterTypes.add(Place.TYPE_ADMINISTRATIVE_AREA_LEVEL_2);
+
+
+        Places.GeoDataApi.getAutocompletePredictions(mGoogleApiClient, constraint, bounds,
+                AutocompleteFilter.create(filterTypes))
+            .setResultCallback(new ResultCallback<AutocompletePredictionBuffer>() {
+                @Override
+                public void onResult(AutocompletePredictionBuffer buffer) {
+                    if (buffer == null) return;
+                    if (buffer.getStatus().isSuccess()) {
+                        mPlaces = new ArrayList<>();
+                        for (AutocompletePrediction prediction: buffer) {
+                            mPlaces.add(new AutoCompletePlace(prediction.getPlaceId(),
+                                    prediction.getDescription()));
+                        }
+                        mPlaceAutoSuggestAdapter.notifyDataSetChanged();
+                    }
+                    buffer.release();
+                }
+            });
+    }
+
+    private static class AutoCompletePlace {
+        private String placeId;
+        private String description;
+
+        public AutoCompletePlace(String placeId, String description) {
+            this.placeId = placeId;
+            this.description = description;
+        }
+
+        public String getPlaceId() {
+            return placeId;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return description;
+        }
     }
 }
---
+++ app/src/main/java/com/bigbasket/mobileapp/interfaces/PlaceAutoSuggestListener.java
@@ -0,0 +1,5 @@
+package com.bigbasket.mobileapp.interfaces;
+
+public interface PlaceAutoSuggestListener {
+    void displaySuggestion(String constraint);
+}
