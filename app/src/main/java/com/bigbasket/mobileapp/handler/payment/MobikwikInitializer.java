2752fa148c880629eaaafdcc6a11ffa0e416744f
jugal <jugal@bigbasket.com>
2015-09-01 11:59:09 +0530

    Mobikwik fix

--- app/src/main/java/com/bigbasket/mobileapp/handler/payment/MobikwikInitializer.java
+++ app/src/main/java/com/bigbasket/mobileapp/handler/payment/MobikwikInitializer.java
@@ -3,6 +3,8 @@ package com.bigbasket.mobileapp.handler.payment;
 import android.app.Activity;
 import android.content.Intent;
 
+import com.bigbasket.mobileapp.R;
+import com.bigbasket.mobileapp.activity.base.BaseActivity;
 import com.bigbasket.mobileapp.util.Constants;
 
 import java.util.HashMap;
@@ -20,8 +22,11 @@ public class MobikwikInitializer {
         for (Map.Entry<String, String> entry : paymentParams.entrySet()) {
             walletIntent.putExtra(entry.getKey(), entry.getValue());
         }
-        walletIntent.putExtra(Constants.MOBIKWIK_MODE, Constants.MOBIKWIK_MODE_VALUE);
+        String sdkMode = ((BaseActivity) ctx).getCurrentActivity().getResources().getString(R.string.mobikwik_mode);
+        walletIntent.putExtra(Constants.MOBIKWIK_MODE, "1");
+        String sdkSign = ((BaseActivity) ctx).getCurrentActivity().getResources().getString(R.string.mobikwik_sdk_sign);
         walletIntent.putExtra(Constants.MOBIKWIK_SDK_SIGN, Constants.MOBIKWIK_SDK_SIGN_VALUE);
+
         walletIntent.putExtra(Constants.MOBIKWIK_PAYMENT_OPTION, Constants.MOBIKWIK_PAYMENT_MW);
         walletIntent.putExtra(Constants.MOBIKWIK_REDIRECT_ANDROID_CLASS_NAME,
                 Constants.MOBIKWIK_RESPONSE_HANDLER_CLASS);
