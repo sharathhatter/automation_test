407522bcdce575f31a4857a3b82185ef15423ce4
Siddharth Srivastava <sidmkp96@gmail.com>
2015-10-21 00:26:42 +0530

    BB-3962 : Fixed text in address-list dropdown

--- app/src/main/java/com/bigbasket/mobileapp/adapter/account/AddressSummaryDropdownAdapter.java
+++ app/src/main/java/com/bigbasket/mobileapp/adapter/account/AddressSummaryDropdownAdapter.java
@@ -9,7 +9,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.bigbasket.mobileapp.R;
@@ -19,7 +19,7 @@ import com.bigbasket.mobileapp.util.FontHolder;
 
 import java.util.ArrayList;
 
-public class AddressSummaryDropdownAdapter extends BaseAdapter {
+public class AddressSummaryDropdownAdapter<T extends AddressSummary> extends ArrayAdapter<T> {
 
     // Spinner in API 21 and onwards doesn't support getItemViewType(),
     // hence using a dirty implementation in getView(),
@@ -28,34 +28,26 @@ public class AddressSummaryDropdownAdapter extends BaseAdapter {
     public static final int VIEW_TYPE_ADDRESS = 0;
     public static final int VIEW_TYPE_CHANGE = 1;
 
-    private ArrayList<AddressSummary> addressSummaries;
+    private ArrayList<T> addressSummaries;
     private String changeAddressTxt;
-    private Context context;
     private Typeface faceRobotLight;
     private Typeface faceRobotoRegular;
     private Typeface faceRobotoBold;
 
-    public AddressSummaryDropdownAdapter(ArrayList<AddressSummary> addressSummaries,
-                                         String changeAddressTxt, Context context) {
+    public AddressSummaryDropdownAdapter(Context context, int resource, ArrayList<T> addressSummaries,
+                                         String changeAddressTxt) {
+        super(context, resource, addressSummaries);
         this.addressSummaries = addressSummaries;
         this.changeAddressTxt = changeAddressTxt;
-        this.context = context;
-        this.faceRobotLight = FontHolder.getInstance(context).getFaceRobotoLight();
-        this.faceRobotoRegular = FontHolder.getInstance(context).getFaceRobotoRegular();
-        this.faceRobotoBold = FontHolder.getInstance(context).getFaceRobotoBold();
+        FontHolder fontHolder = FontHolder.getInstance(context);
+        this.faceRobotLight = fontHolder.getFaceRobotoLight();
+        this.faceRobotoRegular = fontHolder.getFaceRobotoRegular();
+        this.faceRobotoBold = fontHolder.getFaceRobotoBold();
     }
 
     @Override
     public int getCount() {
         return addressSummaries.size() + 1;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        if (getSpinnerViewType(position) == VIEW_TYPE_ADDRESS) {
-            return addressSummaries.get(position);
-        }
-        return changeAddressTxt;
     }
 
     public int getSpinnerViewType(int position) {
@@ -71,56 +63,79 @@ public class AddressSummaryDropdownAdapter extends BaseAdapter {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public View getView(int position, View convertView, ViewGroup parent) {
         if (getSpinnerViewType(position) == VIEW_TYPE_ADDRESS) {
-            AddressSummary addressSummary = addressSummaries.get(position);
-            AddressViewHolder addressViewHolder;
-            if (convertView == null || convertView.getTag() == null) {
-                convertView = LayoutInflater.from(context)
-                        .inflate(R.layout.uiv3_change_address_spinner_row, parent, false);
-                addressViewHolder = new AddressViewHolder(convertView);
-                convertView.setTag(addressViewHolder);
-            } else {
-                addressViewHolder = (AddressViewHolder) convertView.getTag();
-            }
-            String nick = addressSummary.getAddressNickName();
-            /************Setting different color for gingerbread and below******/
-
-            TextView txtCityName = addressViewHolder.getTxtCityName();
-            TextView txtAreaName = addressViewHolder.getTxtAreaName();
-            TextView txtSlotTime = addressViewHolder.getTxtSlotTime();
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
-                txtAreaName.setTextColor(context.getResources().getColor(R.color.dark_black));
-                txtCityName.setTextColor(context.getResources().getColor(R.color.dark_black));
-                txtSlotTime.setTextColor(context.getResources().getColor(R.color.dark_black));
-            }
-            if (TextUtils.isEmpty(nick)) {
-                txtAreaName.setText(addressSummary.getArea());
-            } else {
-                SpannableString spannableString = new SpannableString(nick + " - " +
-                        addressSummary.getArea());
-                spannableString.setSpan(new CustomTypefaceSpan("", faceRobotoBold), 0,
-                        nick.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-                txtAreaName.setText(spannableString);
-            }
-            txtCityName.setText(addressSummary.getCityName());
-            String slot = addressSummary.getSlot();
-            if (!TextUtils.isEmpty(slot)) {
-                txtSlotTime.setText(slot);
-                txtSlotTime.setVisibility(View.VISIBLE);
-            } else {
-                txtSlotTime.setVisibility(View.GONE);
-            }
+            convertView = getAddressRow(false, position, convertView, parent);
         } else {
-            convertView = LayoutInflater.from(context)
-                    .inflate(R.layout.uiv3_change_city_spinner_row, parent, false);
-            TextView txtChangeCity = (TextView) convertView;
-            txtChangeCity.setTypeface(faceRobotoRegular);
-            txtChangeCity.setText(changeAddressTxt);
-            /************Setting different color for gingerbread and below******/
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
-                txtChangeCity.setTextColor(context.getResources().getColor(R.color.dark_black));
-            }
+            convertView = getChangeAddressRow(parent);
+        }
+        return convertView;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        if (getSpinnerViewType(position) == VIEW_TYPE_ADDRESS) {
+            convertView = getAddressRow(true, position, convertView, parent);
+        } else {
+            convertView = getChangeAddressRow(parent);
+        }
+        return convertView;
+    }
+
+    private View getChangeAddressRow(ViewGroup parent) {
+        View convertView = LayoutInflater.from(getContext())
+                .inflate(R.layout.uiv3_change_city_spinner_row, parent, false);
+        TextView txtChangeCity = (TextView) convertView;
+        txtChangeCity.setTypeface(faceRobotoRegular);
+        txtChangeCity.setText(changeAddressTxt);
+        /************Setting different color for gingerbread and below******/
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
+            txtChangeCity.setTextColor(getContext().getResources().getColor(R.color.dark_black));
+        }
+        return convertView;
+    }
+
+    @SuppressWarnings("unchecked")
+    private View getAddressRow(boolean showSlotTime, int position, View convertView,
+                               ViewGroup parent) {
+        AddressSummary addressSummary = addressSummaries.get(position);
+        AddressViewHolder addressViewHolder;
+        if (convertView == null || convertView.getTag() == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.uiv3_change_address_spinner_row, parent, false);
+            addressViewHolder = new AddressViewHolder(convertView);
+            convertView.setTag(addressViewHolder);
+        } else {
+            addressViewHolder = (AddressViewHolder) convertView.getTag();
+        }
+        String nick = addressSummary.getAddressNickName();
+        /************Setting different color for gingerbread and below******/
+
+        TextView txtCityName = addressViewHolder.getTxtCityName();
+        TextView txtAreaName = addressViewHolder.getTxtAreaName();
+        TextView txtSlotTime = addressViewHolder.getTxtSlotTime();
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
+            txtAreaName.setTextColor(getContext().getResources().getColor(R.color.dark_black));
+            txtCityName.setTextColor(getContext().getResources().getColor(R.color.dark_black));
+            txtSlotTime.setTextColor(getContext().getResources().getColor(R.color.dark_black));
+        }
+        if (TextUtils.isEmpty(nick)) {
+            txtAreaName.setText(addressSummary.getArea());
+        } else {
+            SpannableString spannableString = new SpannableString(nick + " - " +
+                    addressSummary.getArea());
+            spannableString.setSpan(new CustomTypefaceSpan("", faceRobotoBold), 0,
+                    nick.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+            txtAreaName.setText(spannableString);
+        }
+        txtCityName.setText(addressSummary.getCityName());
+        String slot = addressSummary.getSlot();
+        if (!TextUtils.isEmpty(slot) && showSlotTime) {
+            txtSlotTime.setText(slot);
+            txtSlotTime.setVisibility(View.VISIBLE);
+        } else {
+            txtSlotTime.setVisibility(View.GONE);
         }
         return convertView;
     }
