1a1c7bd9b28f39e54e2674db388617d7e8a322e6
Muniraju N R <muniraju.r@bigbasket.com>
2015-11-05 20:09:43 +0530

    BB-6827: Enable developer options in the android app

---
+++ app/src/debug/java/com/bigbasket/mobileapp/devconfig/OnServerChangeListener.java
@@ -0,0 +1,8 @@
+package com.bigbasket.mobileapp.devconfig;
+
+/**
+ * Created by bigbasket on 5/11/15.
+ */
+public interface OnServerChangeListener {
+    public void onServerChanged(String newServer);
+}
--- app/src/main/java/com/bigbasket/mobileapp/util/MobileApiUrl.java
+++ app/src/main/java/com/bigbasket/mobileapp/util/MobileApiUrl.java
@@ -1,11 +1,25 @@
 package com.bigbasket.mobileapp.util;
 
+import android.content.Context;
+
+import com.bigbasket.mobileapp.BuildConfig;
+import com.bigbasket.mobileapp.R;
+import com.bigbasket.mobileapp.devconfig.DeveloperConfigs;
+
 public final class MobileApiUrl {
-
-    public static final String DOMAIN = "https://bigbasket.com/";
-
-    public static final String URL = DOMAIN + "mapi/v2.3.0";
+    private static final String API_PATH = "/mapi/v2.3.0";
 
     private MobileApiUrl() {
+    }
+
+    public static String getMobileApiUrl(Context context){
+        if(BuildConfig.DEBUG) {
+            if(DeveloperConfigs.isDeveloper(context)) {
+                return DeveloperConfigs.getMapiServerAddress(context) + API_PATH;
+            }
+        }
+
+        return context.getString(R.string.pref_default_server_address) + API_PATH;
+
     }
 }
