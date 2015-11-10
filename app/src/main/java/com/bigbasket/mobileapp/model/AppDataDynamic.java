dee5ad1c8fdb4805422cef16e2b41be7f6b6b9d3
jugal joshi <jugal.joshi1735@gmail.com>
2015-11-10 15:16:50 +0530

    get app data change for store info

--- app/src/main/java/com/bigbasket/mobileapp/model/AppDataDynamic.java
+++ app/src/main/java/com/bigbasket/mobileapp/model/AppDataDynamic.java
@@ -189,6 +189,7 @@ public class AppDataDynamic {
         return storeAvailabilityMap;
     }
 
+    @Nullable
     public ArrayList<HashMap<String, SpecialityStoresInfoModel>> getSpecialityStoresInfo() {
         return specialityStoresInfo;
     }
