ec4d72e6e26141ffc17a546884c30561e0c26e2e
Siddharth Srivastava <sidmkp96@gmail.com>
2015-12-28 16:58:04 +0530

    BB-7886 : Fix for ANR caused by pin-code insertion & dynamic-screen deletion

--- app/src/main/java/com/bigbasket/mobileapp/service/AreaPinInfoIntentService.java
+++ app/src/main/java/com/bigbasket/mobileapp/service/AreaPinInfoIntentService.java
@@ -1,23 +1,17 @@
 package com.bigbasket.mobileapp.service;
 
 import android.app.IntentService;
-import android.content.ContentProviderOperation;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.content.OperationApplicationException;
-import android.os.RemoteException;
 import android.util.Log;
 
 import com.bigbasket.mobileapp.adapter.account.AreaPinInfoDbHelper;
-import com.bigbasket.mobileapp.adapter.db.DatabaseContentProvider;
-import com.bigbasket.mobileapp.adapter.db.DatabaseHelper;
 import com.bigbasket.mobileapp.apiservice.BigBasketApiAdapter;
 import com.bigbasket.mobileapp.apiservice.BigBasketApiService;
 import com.bigbasket.mobileapp.apiservice.models.response.ApiResponse;
 import com.bigbasket.mobileapp.apiservice.models.response.GetAreaInfoResponse;
 import com.bigbasket.mobileapp.managers.CityManager;
 import com.bigbasket.mobileapp.model.account.City;
-import com.crashlytics.android.Crashlytics;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -83,11 +77,9 @@ public class AreaPinInfoIntentService extends IntentService {
             }
         }
         if (success && downloadedDataMap.size() > 0) {
-            DatabaseHelper.getInstance(this).open(this);
-            DatabaseHelper.db.beginTransaction();
-            ArrayList<ContentProviderOperation> contentProviderOperations = new ArrayList<>();
-            contentProviderOperations.add(ContentProviderOperation
-                    .newDelete(AreaPinInfoDbHelper.CONTENT_URI).build());
+            Log.d(TAG, "Inserting pin-codes");
+            long startTime = System.currentTimeMillis();
+            ArrayList<ContentValues> contentValues = new ArrayList<>();
 
             for (Map.Entry<City, HashMap<String, ArrayList<String>>> pinCodeEntrySet :
                     downloadedDataMap.entrySet()) {
@@ -98,30 +90,22 @@ public class AreaPinInfoIntentService extends IntentService {
                     ArrayList<String> areas = pincodeAreaEntrySet.getValue();
                     for (String areaName : areas) {
                         ContentValues cv = new ContentValues();
-
                         cv.put(AreaPinInfoDbHelper.COLUMN_PIN, pinCode);
                         cv.put(AreaPinInfoDbHelper.COLUMN_AREA, areaName);
                         cv.put(AreaPinInfoDbHelper.COLUMN_CITY, city.getName());
                         cv.put(AreaPinInfoDbHelper.COLUMN_CITY_ID, city.getId());
-                        contentProviderOperations
-                                .add(ContentProviderOperation
-                                        .newInsert(AreaPinInfoDbHelper.CONTENT_URI)
-                                        .withValues(cv)
-                                        .build());
+                        contentValues.add(cv);
                     }
                 }
             }
-            try {
-                getContentResolver().applyBatch(DatabaseContentProvider.AUTHORITY,
-                        contentProviderOperations);
+            int rowCount = getContentResolver().bulkInsert(AreaPinInfoDbHelper.CONTENT_URI,
+                    contentValues.toArray(new ContentValues[contentValues.size()]));
+            if (rowCount > 0) {
+                long elapsedTime = System.currentTimeMillis() - startTime;
                 CityManager.setAreaPinInfoDate(this);
-                DatabaseHelper.db.setTransactionSuccessful();
-                Log.d(TAG, "Successfully fetched all pin-codes");
-            } catch (RemoteException | OperationApplicationException e) {
-                Log.e(TAG, "Failed to insert pin-codes", e);
-                Crashlytics.logException(e);
-            } finally {
-                DatabaseHelper.db.endTransaction();
+                Log.d(TAG, "Successfully fetched all pin-codes in " + elapsedTime + " milliseconds");
+            } else {
+                Log.e(TAG, "Failed to insert pin-codes");
             }
         }
     }
